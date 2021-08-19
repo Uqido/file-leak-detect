25e0f5f31bbcad1721071770071282fd1f6bbb52
Dino <dino.puller@uqido.com>
2021-08-19 15:44:04 +0200

    Fix for rake

--- lib/file/leak/detect.rb
+++ lib/file/leak/detect.rb
@@ -10,47 +10,49 @@ module FileLeakDetect
   end
 end
 
-FileLeakDetect.debug = false
-FileLeakDetect.enabled = true
-FileLeakDetect.global_opened_files = {}
+if defined?(RSpec)
+  FileLeakDetect.debug = false
+  FileLeakDetect.enabled = true
+  FileLeakDetect.global_opened_files = {}
 
-RSpec.configure do |config|
-  config.before :each do
-    next if !FileLeakDetect.enabled
+  RSpec.configure do |config|
+    config.before :each do
+      next if !FileLeakDetect.enabled
 
-    allow(File).to receive(:open).and_wrap_original do |m, *args, &block|
-      file = args[0]
-      file = file.path if file.is_a?(File)
-      file = file.to_s if file.is_a?(Pathname)
-      FileLeakDetect.global_opened_files[file] = 0 if !FileLeakDetect.global_opened_files.include?(file)
-      FileLeakDetect.global_opened_files[file] = FileLeakDetect.global_opened_files[file] + 1
-      puts "opening file #{file}" if FileLeakDetect.debug
-      m.call(*args, &block)
-    end
-
-    allow_any_instance_of(File).to receive(:close).and_wrap_original do |m, *args, &block|
-      begin
+      allow(File).to receive(:open).and_wrap_original do |m, *args, &block|
+        file = args[0]
+        file = file.path if file.is_a?(File)
+        file = file.to_s if file.is_a?(Pathname)
+        FileLeakDetect.global_opened_files[file] = 0 if !FileLeakDetect.global_opened_files.include?(file)
+        FileLeakDetect.global_opened_files[file] = FileLeakDetect.global_opened_files[file] + 1
+        puts "opening file #{file}" if FileLeakDetect.debug
         m.call(*args, &block)
-      ensure
-        file = m.receiver.path
-        # It's seems common behaviour to close a just closed file
-        if FileLeakDetect.global_opened_files.has_key?(file)
-          puts "closing file #{file}" if FileLeakDetect.debug
-          FileLeakDetect.global_opened_files[file] = FileLeakDetect.global_opened_files[file] - 1
-          FileLeakDetect.global_opened_files.delete(file) if FileLeakDetect.global_opened_files[file].zero?
+      end
+
+      allow_any_instance_of(File).to receive(:close).and_wrap_original do |m, *args, &block|
+        begin
+          m.call(*args, &block)
+        ensure
+          file = m.receiver.path
+          # It's seems common behaviour to close a just closed file
+          if FileLeakDetect.global_opened_files.has_key?(file)
+            puts "closing file #{file}" if FileLeakDetect.debug
+            FileLeakDetect.global_opened_files[file] = FileLeakDetect.global_opened_files[file] - 1
+            FileLeakDetect.global_opened_files.delete(file) if FileLeakDetect.global_opened_files[file].zero?
+          end
         end
       end
     end
-  end
 
-  config.after :each do
-    begin
-      next if !FileLeakDetect.enabled
+    config.after :each do
+      begin
+        next if !FileLeakDetect.enabled
 
-      # Check that all files are closed
-      expect(FileLeakDetect.global_opened_files).to eq({}), 'Lazy programmer, not all files was closed!!!'
-    ensure
-      FileLeakDetect.global_opened_files = {}
+        # Check that all files are closed
+        expect(FileLeakDetect.global_opened_files).to eq({}), 'Lazy programmer, not all files was closed!!!'
+      ensure
+        FileLeakDetect.global_opened_files = {}
+      end
     end
   end
 end
